92c572b6662b268ffdcde133bb6bc667b4ca5830
Your Name <you@example.com>
2019-01-28 14:53:59 +0800

    计算设备宽度rem布局1rem=50px

--- plants/src/store/index.js
+++ plants/src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import plant from './Plant'
+import community from './Community'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     plant,
+    community
   }
 })
