16ad7077b46cf1751c55e9a5c39f861653d6e3f9
a6831084 <a6831084@163.com>
2019-02-12 21:03:14 +0800

    首页布局，明天详情跳转

--- plants/src/pages/Plant.vue
+++ plants/src/pages/Plant.vue
@@ -9,7 +9,12 @@
         <div ref="ef" class="custom-indicator" slot="indicator">{{ current }}/3</div>
       </van-swipe>
       <ul class="mains">
-        <li class="main" v-for="(item,index) in plant.plantList" :key="index">
+        <li
+          class="main"
+          @click="MainsAction(item.index)"
+          v-for="(item,index) in plant.plantList"
+          :key="index"
+        >
           <span class="decPlant-one">{{item.dec}}</span>
           <img class="imgPlant-one" v-lazy="item.img" alt>
           <span class="actorbty">{{actor}}</span>
@@ -17,6 +22,43 @@
           <span class="lovePlant-one">{{item.love}}</span>
         </li>
       </ul>
+
+      <div class="main1">
+        <span class="decPlant-two">{{ plant.plantList1.dec}}</span>
+        <img class="imgPlant-two" v-lazy="plant.plantList1.img" alt>
+        <span class="actor-two">{{actor1}}</span>
+        <span class="commentPlant-two">{{plant.plantList1.comments}}</span>
+        <span class="lovePlant-two">{{plant.plantList1.loves}}</span>
+      </div>
+
+      <ul class="main2">
+        <li class="main2-list" v-for="(item,index) in plant.plantList2" :key="index">
+          <img class="imgPlant-three" v-lazy="item.img" alt>
+          <span class="decPlant-three">{{ item.dec}}</span>
+          <span class="actor-three">{{actor1}}</span>
+          <span class="commentPlant-three">{{item.comments}}</span>
+          <span class="lovePlant-three">{{item.loves}}</span>
+        </li>
+      </ul>
+
+      <ul class="main2">
+        <li class="main2-list" v-for="(item,index) in plant.plantList2" :key="index">
+          <img class="imgPlant-three" v-lazy="item.img" alt>
+          <span class="decPlant-three">{{ item.dec}}</span>
+          <span class="actor-three">{{actor1}}</span>
+          <span class="commentPlant-three">{{item.comments}}</span>
+          <span class="lovePlant-three">{{item.loves}}</span>
+        </li>
+      </ul>
+
+      <div class="main1">
+        <span class="decPlant-two">{{ plant.plantList1.dec}}</span>
+        <img class="imgPlant-two" v-lazy="plant.plantList1.img" alt>
+        <br>
+        <span class="actor-two">{{actor1}}</span>
+        <span class="commentPlant-two">{{plant.plantList1.comments}}</span>
+        <span class="lovePlant-two">{{plant.plantList1.loves}}</span>
+      </div>
     </scroller>
   </div>
 </template>
@@ -29,7 +71,8 @@ export default {
     return {
       current: 1,
       color: ["red", "white", "yellow"],
-      actor: "编者最美"
+      actor: "编者最美",
+      actor1: "鹿石花艺Sikastone"
     };
   },
   methods: {
@@ -52,6 +95,9 @@ export default {
       } else {
         return getRandomColor();
       }
+    },
+    MainsAction() {
+      this.$router.push("/plant/mains");
     }
   },
   created() {
@@ -91,38 +137,81 @@ export default {
   .mains {
     width: 7.5rem;
     position: relative;
+    .main {
+      height: 2.5rem;
+      border: 1px solid #ccc;
+      margin-top: 1rem;
+      background: lightcyan;
+      .decPlant-one {
+        font-size: 0.43rem;
+        position: absolute;
+        width: 3.75rem;
+      }
+      .actorbty {
+        position: absolute;
+        line-height: 4.5rem;
+        right: 6.2rem;
+      }
+      .commentPlant-one {
+        position: absolute;
+        line-height: 4.5rem;
+        right: 3.9rem;
+      }
+      .lovePlant-one {
+        position: absolute;
+        line-height: 4.5rem;
+        right: 5.1rem;
+      }
+      .imgPlant-one {
+        width: 2.9rem;
+        height: 2.5rem;
+        position: absolute;
+        right: 0;
+      }
+    }
   }
-  .main {
+  .main2 {
+    display: flex;
+    margin-top: 0.5rem;
+    width: 3.55rem;
     height: 2.5rem;
-    border: 1px solid #ccc;
-    margin-top: 1rem;
-    background: lightcyan;
+
+    .main2-list {
+      .imgPlant-three {
+        width: 3.75rem;
+      }
+      .commentPlant-three {
+      }
+    }
   }
-  .decPlant-one {
-    font-size: 0.43rem;
-    position: absolute;
-    width: 3.75rem;
-  }
-  .actorbty {
-    position: absolute;
-    line-height: 4.5rem;
-    right: 6.2rem;
-  }
-  .commentPlant-one {
-    position: absolute;
-    line-height: 4.5rem;
-    right: 3.9rem;
-  }
-  .lovePlant-one {
-    position: absolute;
-    line-height: 4.5rem;
-    right: 5.1rem;
-  }
-  .imgPlant-one {
-    width: 2.9rem;
-    height: 2.5rem;
-    position: absolute;
-    right: 0;
+  .main1 {
+    height: 4.5rem;
+    margin-top: 0.5rem;
+    position: relative;
+    .decPlant-two {
+      font-size: 0.4rem;
+    }
+    .actor-two {
+      position: absolute;
+      left: 0;
+      z-index: 1;
+      font-size: 0.3rem;
+      color: #333;
+    }
+    .commentPlant-two {
+      position: absolute;
+      right: 1.5rem;
+      z-index: 1;
+      font-size: 0.3rem;
+      color: #333;
+    }
+    .lovePlant-two {
+      position: absolute;
+      right: 0;
+      z-index: 1;
+      font-size: 0.3rem;
+      color: #333;
+    }
   }
 }
 </style>
